d7f5bd7f989e8a2d3fe24681be17a69fcde9c2f6
manonforsteri <manon.ghislain@gmail.com>
2021-10-25 13:05:44 +0200

    Update code_carto.R

--- code_carto.R
+++ code_carto.R
@@ -1,16 +1,20 @@
-#installer et ouvrir le package nécessaire
-#un redémarrage de R peut être nécessaire
+#installer et ouvrir le package nÃ©cessaire
+#un redÃ©marrage de R peut Ãªtre nÃ©cessaire
 install.packages("birdring")
 library(birdring)
 
-#crér un dossier nommé "map bird ring"  sur votre ordinateur dans "mes documents"
-#et y mettre le fichier de données, puis l'ouvrir : 
+#si vous recevez un message d'erreur vous indiquant qu'il manque un package ("aucun package "..." est trouvÃ©), 
+#installez ce package avec la formule install.packages("nom du package"), puis relancez le script depuis le dÃ©but
+#il est possible que l'opÃ©ration soit Ã  rÃ©pÃ©ter plusiers fois si plusieurs packages sont manquants.
+
+#crÃ©r un dossier nommÃ© "map bird ring"  sur votre ordinateur dans "mes documents"
+#et y mettre le fichier de donnÃ©es, puis l'ouvrir : 
 setwd("~/map bird ring")
 
-#Créer un fichier en .CSV avec toutes vos données baguages/controles
+#CrÃ©er un fichier en .CSV avec toutes vos donnÃ©es baguages/controles
 #Pour simplifier l'importation, ne gardez dans ce fichier que les colonnes : 
 # ACTION CENTRE       BAGUE       DATE ESPECE PAYS      LAT       LON
-#vérifier qu'il n'y a pas de cases vides dans le fichier excel
+#vÃ©rifier qu'il n'y a pas de cases vides dans le fichier excel
 
 #Importer le fichier
 data_bird<-read.table("data_bird.csv", header = TRUE, sep = ";" ,
@@ -27,19 +31,19 @@ data_bird$metal.ring.info[data_bird$ACTION=="R"]<-4
 data_bird$ring<-data_bird$BAGUE
 data_bird$scheme<-data_bird$CENTRE
 
-#sélection des données présentes avec au moins un controle ou une reprise
+#sÃ©lection des donnÃ©es prÃ©sentes avec au moins un controle ou une reprise
 bird_recapt<-as.data.frame(table(data_bird$BAGUE))
 bird_recapt2<-bird_recapt[which(bird_recapt$Freq>1),]
 data_bird<-data_bird[which(data_bird$BAGUE %in% bird_recapt2$Var1),]
 
-#créer un tableau et sélectionner une espèce
+#crÃ©er un tableau et sÃ©lectionner une espÃ¨ce
 acrsci<-data_bird[which(data_bird$ESPECE=="ACRSCI"),]
 embsch<-data_bird[which(data_bird$ESPECE=="EMBSCH"),]
 sylatr<-data_bird[which(data_bird$ESPECE=="SYLATR"),]
 
 
-#tracer la carte (il faut juste modifier le nom de l'espèce)
-#ça peut prendre un petit moment si vous avez beaucoup de données
+#tracer la carte (il faut juste modifier le nom de l'espÃ¨ce)
+#Ã§a peut prendre un petit moment si vous avez beaucoup de donnÃ©es
 draw.recmap(sylatr, points=1, lines=1, pcol="black", lcol="red",
             mercator=TRUE, bbox=c(-20, 40, 0, 70),
             projection= "mercator", border= "gray")
@@ -47,9 +51,9 @@ draw.recmap(sylatr, points=1, lines=1, pcol="black", lcol="red",
 
 #Bonus : pour "recarder" les cartes aux bonnes latitudes et longitudes
 #vous pouvez utiliser les fonctions ci dessous, pour connaitre les 
-#latitudes et longitudes min et max de votre jeu de données, 
-#et garder les mêmes dimensions pour toutes les cartes
-#il faut ensuite les modifier dans la formule ci-dessus dans le paramètre "bbox" :
+#latitudes et longitudes min et max de votre jeu de donnÃ©es, 
+#et garder les mÃªmes dimensions pour toutes les cartes
+#il faut ensuite les modifier dans la formule ci-dessus dans le paramÃ¨tre "bbox" :
 #avec bbox=c(min lon, max long, min lat, max lat)
 
 min(data_bird$lat)
